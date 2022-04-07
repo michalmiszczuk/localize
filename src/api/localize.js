4e5b7609dc3f92b03f987a305954a9a7d35a5ab3
Michal_Miszczuk <michal.miszczuk.webdev@gmail.com>
2022-04-07 08:45:05 +0200

    axios fix 2

--- src/api/localize.js
+++ src/api/localize.js
@@ -3,16 +3,19 @@ import axios from "axios";
 const apiKey = process.env.REACT_APP_API_KEY;
 
 async function findCoordinates(ip) {
-  const {data} = await axios.get(`http://api.ipstack.com/${ip}?access_key=${apiKey}&fields=main&hostname=1`, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+  const {data} = await axios.get(
+    `http://api.ipstack.com/${ip}/?access_key=${apiKey}&fields=main&hostname=1`,
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
   return data;
 }
 
 async function findMyCoordinates() {
-  const {data} = await axios.get(`http://api.ipstack.com/check?access_key=${apiKey}&fields=main`);
+  const {data} = await axios.get(`http://api.ipstack.com/check/?access_key=${apiKey}&fields=main`);
   console.log(data);
   return data;
 }
