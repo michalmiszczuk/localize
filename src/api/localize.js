cc30c29e5a3c46ad48e3a6fe7548e48e45d3abd3
Michal_Miszczuk <michal.miszczuk.webdev@gmail.com>
2022-04-07 12:05:35 +0200

    version 1.1

--- src/api/localize.js
+++ src/api/localize.js
@@ -3,14 +3,7 @@ import axios from "axios";
 const apiKey = process.env.REACT_APP_API_KEY;
 
 async function findCoordinates(ip) {
-  const {data} = await axios.get(
-    `http://api.ipstack.com/${ip}/?access_key=${apiKey}&fields=main&hostname=1`,
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    }
-  );
+  const {data} = await axios.get(`http://api.ipstack.com/${ip}/?access_key=${apiKey}&fields=main&hostname=1`);
   return data;
 }
 
